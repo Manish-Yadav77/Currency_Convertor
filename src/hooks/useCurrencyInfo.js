d86da70315d0b0367966bdd6e288fba0ce61f78c
Manish-Yadav77 <manishkyadav969@gmail.com>
2025-07-21 19:34:05 +0530

    Update useCurrencyInfo.js

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,37 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-function useCurrencyInfo(currency) {
-  const [data, setData] = useState({});
-  const [error, setError] = useState(null);
+function useCurrencyInfo(baseCurrency) {
+  const [data, setData] = useState({})
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     const fetchCurrencyData = async () => {
       try {
         const response = await fetch(
-          `https://api.currencyapi.com/v3/latest?apikey=cur_live_7tbJChV0QCxQaiJX6S2tT3VIrYy3eZorFoKvCJrX`
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch currency data");
-        }
-
-        const result = await response.json();
-        if (result.data && result.data[currency]) {
-          setData(result.data);
+          `https://api.currencyapi.com/v3/latest?apikey=cur_live_7tbJChV0QCxQaiJX6S2tT3VIrYy3eZorFoKvCJrX&base_currency=${baseCurrency}`
+        )
+        const result = await response.json()
+        if (result.data) {
+          setData(result.data)
         } else {
-          setError(`Currency data for '${currency}' not available.`);
+          setError('Failed to fetch currency rates.')
         }
       } catch (err) {
-        setError(err.message);
+        setError(err.message)
       }
-    };
-
-    if (currency) {
-      fetchCurrencyData();
     }
-  }, [currency]);
 
-  return { data, error };
+    if (baseCurrency) fetchCurrencyData()
+  }, [baseCurrency])
+
+  return { data, error }
 }
 
-export default useCurrencyInfo;
-
+export default useCurrencyInfo
